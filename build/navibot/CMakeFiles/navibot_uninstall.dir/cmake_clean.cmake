58d4355ada53f96199fb455459d1584a47944980
Dwbcan <dew.bhaumik8@gmail.com>
2023-04-19 01:52:57 -0700

    build: Deleted unecessary build/log files and updated .gitignore

--- build/navibot/CMakeFiles/navibot_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/navibot_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/navibot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
