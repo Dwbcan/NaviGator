c7d4fd4a079d7956979b98c184b7c06c9b6fcd59
Dwbcan <dew.bhaumik8@gmail.com>
2023-04-29 22:15:52 -0400

    feat: Initialized appropriate subscribers and publishers for A* path planning node

--- src/ball_tracking/src/plan_path.cpp
+++ src/ball_tracking/src/plan_path.cpp
@@ -1,5 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
-#include "nav2_msgs/action/compute_path_to_pose.hpp"
+#include "nav2_msgs/srv/compute_path_to_pose.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
 #include "geometry_msgs/msg/point_stamped.hpp"
 #include <memory>
@@ -9,9 +9,16 @@ class PlanPath : public rclcpp::Node
     public:
         PlanPath() : rclcpp::Node("plan_path")
         {
-            
+            costmap_subscriber_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
+            "/global_costmap/costmap",
+            rclcpp::QoS(rclcpp::KeepLast(10)),
+            std::bind(&PlanPath::costmap_callback, this, std::placeholders::_1));
+
+            nav2_client_ = this->create_client<nav2_msgs::srv::ComputePathToPose>("compute_path_to_pose");
         }
     private:
+        rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_subscriber_;
+        rclcpp::Client<nav2_msgs::srv::ComputePathToPose>::SharedPtr nav2_client_;
 };
 
 int main(int argc, char * argv[])
