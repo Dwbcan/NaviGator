c93db7a3e6f48ead8057196731430d84447333c0
Dwbcan <dew.bhaumik8@gmail.com>
2023-05-01 01:50:00 -0400

    feat: Initialized subscribers and publishers for follow_ball node

--- src/ball_tracking/src/follow_ball.cpp
+++ src/ball_tracking/src/follow_ball.cpp
@@ -7,10 +7,23 @@ class FollowBall : public rclcpp::Node
     public:
         FollowBall() : Node("follow_ball")
         {
-            
+            // Create subscriber to localize navibot using Adaptive Monte Carlo Localization (AMCL)
+            pose_sub_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
+            "/amcl_pose", 10, std::bind(&FollowBall::amcl_callback, this, std::placeholders::_1));
+
+            // Create subscriber that receives A* path from planned_path topic
+            path_sub_ = this->create_subscription<nav_msgs::msg::Path>(
+            "/planned_path", 10, std::bind(&FollowBall::path_callback, this, std::placeholders::_1));
+
+            // Create publisher to publish command velocity and make navibot follow A* path
+            cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
         }
 
     private:
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
+        rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+        geometry_msgs::msg::PoseStamped robot_pose_;  // Navibot's current pose
 };
 
 int main(int argc, char * argv[])
